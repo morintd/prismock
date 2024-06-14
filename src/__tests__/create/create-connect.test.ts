19236d250cb1bf2cec743e989c11502c74db360d
Teddy Morin <morin.td@gmail.com>
2024-06-14 22:05:56 +0200

    fix: 🐛 add test for create connect use case

--- src/__tests__/create/create-connect.test.ts
+++ src/__tests__/create/create-connect.test.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
-import { resetDb, seededPosts, simulateSeed } from '../../../testing';
+import { formatEntry, resetDb, seededPosts, seededUsers, simulateSeed } from '../../../testing';
 import { PrismockClient, PrismockClientType } from '../../lib/client';
 
 jest.setTimeout(40000);
@@ -9,12 +9,18 @@ describe('create (connect)', () => {
   let prismock: PrismockClientType;
   let prisma: PrismaClient;
 
+  let realServiceUser: User;
+  let mockServiceUser: User;
+
   beforeAll(async () => {
     await resetDb();
 
     prisma = new PrismaClient();
     prismock = new PrismockClient() as PrismockClientType;
     await simulateSeed(prismock);
+
+    realServiceUser = (await prisma.user.findFirst({ where: { email: seededUsers[0].email } }))!;
+    mockServiceUser = (await prismock.user.findFirst({ where: { email: seededUsers[0].email } }))!;
   });
 
   it('Should create with multiple dependencies and connect to it', async () => {
@@ -91,5 +97,59 @@ describe('create (connect)', () => {
 
     expect(realBlog).toEqual(expected);
     expect(mockBlog).toEqual(expected);
+  });
+
+  it('Should create with dependency + include and connect to it', async () => {
+    const realService = await prisma.service.create({
+      data: {
+        name: 'service-create-connect-include',
+        tags: [],
+        user: {
+          connect: {
+            id: realServiceUser.id,
+          },
+        },
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    const mockService = await prismock.service.create({
+      data: {
+        name: 'service-create-connect-include',
+        tags: [],
+        user: {
+          connect: {
+            id: mockServiceUser.id,
+          },
+        },
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    expect(formatEntry(mockService.user)).toEqual(formatEntry(realService.user));
+
+    const realServiceFind = await prisma.service.findFirst({
+      where: {
+        name: 'service-create-connect-include',
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    const mockServiceFind = await prismock.service.findFirst({
+      where: {
+        name: 'service-create-connect-include',
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    expect(realServiceFind?.user).toEqual(mockServiceFind?.user);
   });
 });
